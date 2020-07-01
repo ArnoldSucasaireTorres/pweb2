d9653c26fa4d7829c2056ff21be5e9aa410926c5
ArnoldSucasaireTorres <asucasairet@unsa.edu.pe>
2020-07-01 09:51:16 -0500

    login v0.2

--- telusko/accounts/views.py
+++ telusko/accounts/views.py
@@ -7,8 +7,18 @@ def login(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'credenciales invalidas')
+            return redirect('login')
+
     else:
-        return render(request, 'register.html')
+        return render(request, 'login.html')
 
 def register(request):
     if request.method == 'POST':
@@ -30,6 +40,7 @@ def register(request):
                 user = User.objects.create_user(username=username,password=password1,email=email,first_name=first_name,last_name=last_name)
                 user.save()
                 print('user created')
+                return redirect('login')
         else:
             messages.info(request, 'La contraseña no coincide')
             return redirect('register')
