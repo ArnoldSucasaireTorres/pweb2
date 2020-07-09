90b34047f70c3f84aa1cccb76b679bf2ff19c32c
ArnoldSucasaireTorres <asucasairet@unsa.edu.pe>
2020-07-08 21:34:48 -0500

    DestinosForm vista

--- telusko/travello/views.py
+++ telusko/travello/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Destination
+from .forms import DestinosForm
 # Create your views here.
 
 
@@ -8,3 +9,10 @@ def index(request):
     dests = Destination.objects.all()
 
     return render(request,"index.html", {'dests': dests})
+
+def Destinos(request):
+    form = DestinosForm()
+    context = {
+        'form': form,
+    }
+    return render(request, '')
